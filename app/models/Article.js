c5554a0ecbf1e791abafdffbdb924993292ba91a
Fiaalii Togiaso <jtogiaso@gmail.com>
2017-12-09 07:40:40 -0800

    Working on Models and Frontend

--- app/models/Article.js
+++ app/models/Article.js
@@ -1,19 +1,13 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
-var NoteSchema = new Schema({
-  // `title` must be of type String
+const ArticleSchema = new Schema({
   title: String,
-  // `body` must be of type String
-  body: String
+  summary: String,
+  url: String
 });
 
-// This creates our model from the above schema, using mongoose's model method
-var Note = mongoose.model("Note", NoteSchema);
+const Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Note model
-module.exports = Note;
+module.exports = Article;
--- app/models/Comment.js
+++ app/models/Comment.js
@@ -1,31 +1,17 @@
 var mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
-
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-var UserSchema = new Schema({
-  // `name` must be unique and of type String
-  name: {
-    type: String,
-    unique: true
+var CommentSchema = new Schema({
+  userName: String,
+  date: {
+    type: Date,
+    default: Date.now
   },
-  // `notes` is an array that stores ObjectIds
-  // The ref property links these ObjectIds to the Note model
-  // This allows us to populate the User with any associated Notes
-  notes: [
-    {
-      // Store ObjectIds in the array
+  text: String,
+  article: {
       type: Schema.Types.ObjectId,
-      // The ObjectIds will refer to the ids in the Note model
-      ref: "Note"
+      ref: "Article"
     }
-  ]
-});
+  });
 
-// This creates our model from the above schema, using mongoose's model method
-var User = mongoose.model("User", UserSchema);
-
-// Export the User model
-module.exports = User;
+var Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
